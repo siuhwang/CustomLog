0cc9b24b5d6f0714cc03f0a3f3236c18da0c66a9
siu <hsiwoo@icloud.com>
2023-09-09 15:15:25 +0900

    Initial Commit

---
+++ CustomLog/CustomLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLogApp.swift
+//  CustomLog
+//
+//  Created by Siu on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
